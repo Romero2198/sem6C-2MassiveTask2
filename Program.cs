a2673beb9ca8cd5b5cddef13b1291b8ebb787dea
Yung.Energy <morskojroma@gmail.com>
2022-07-16 17:53:30 +0300

    Task2

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿double b1 = InputInt("Введите b1: ");
+double k1 = InputInt("Введите k1: ");
+double b2 = InputInt("Введите b2: ");
+double k2 = InputInt("Введите k2: ");
+double x = (b2 - b1) / (k1 - k2);
+double y1 = k1 * x + b1;
+double y2 = k2 * x + b2;
+Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({y1}; {y2})");
+
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
